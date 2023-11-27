2e74557a1e450da1e6a598585efa44343ed6ba81
Nikola Mihaylov <nikolamihaylov@Nikolas-MacBook-Pro.local>
2023-11-27 12:28:42 +0200

    Docker setup

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,8 @@
+if (!process.env.NODE_ENV) {
+    const dotenv = require('dotenv')
+    dotenv.config({ path: __dirname + '/../.env.local'});
+}
+
 export default {
     port: process.env.PORT || 3000,
 }
